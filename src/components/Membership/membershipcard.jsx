a50b902be2280d85440a84b2e9c54c4d44ed4da6
Khushboo Gehlot <khushbugehlot200@gmail.com>
2023-08-29 18:26:08 +0530

    third commit

--- src/components/Membership/membershipcard.jsx
+++ src/components/Membership/membershipcard.jsx
@@ -10,7 +10,7 @@ function Membershipcard() {
 
       setData(res.data.data)
       // console.table(res.data.data);
-    }).catch(err=>console.log(err))
+    }).catch(err => console.log(err))
   }
   function calculateMonthsAndDays(days) {
     const daysInMonth = 30;
@@ -39,53 +39,53 @@ function Membershipcard() {
 
 
           {
-            getData.length>0?
-            getData.map((element) => {
-              if (element.mp_dis_price == 0) {
-                element.mp_dis_price = element.mp_price;
-              }
-
-              let inputDays = 0;
-              inputDays = element.mp_duration
-
-              if (!isNaN(inputDays)) {
-                const result = calculateMonthsAndDays(inputDays);
-                if (result.months == 0) {
-                  var time = `${result.remainingDays} Days`;
-                }
-                else {
-                  time = `${result.months} Month ${result.remainingDays} Days`;
+            getData.length > 0 ?
+              getData.map((element) => {
+                if (element.mp_dis_price == 0) {
+                  element.mp_dis_price = element.mp_price;
                 }
 
-              } else {
-                console.log("Invalid input. Please enter a valid number of days.");
-              }
+                let inputDays = 0;
+                inputDays = element.mp_duration
 
-              return (<div className="col-lg-4 col-md-6 col-12 p-0 my-2 ">
-                <div className='membership_main_col mx-1'> <div className="membership_col1 py-5">
-                  <h4 className='text-white'>{element.mp_name}</h4>
-                  <h4 className='text-white ' id='price'><sup>₹</sup>{element.mp_dis_price}<span>/ month</span></h4>
-                  <h4 id="membership_h4_3"
-                  // className={`${element.mp_price === element.mp_dis_price && "d-none"}`}
-                  ><sup>₹</sup>
-                    {element.mp_price}
-                  </h4>
-                </div>
-                  <div className="membership_col2 py-4">
-                    <ul >
-                      <li>View unlimited profiles</li>
-                      <li>Send unilimited Express interest invites</li>
-                      <li>{time}</li>
-                      <li>View {element?.mp_dir_contact_count} contact for direct communication</li>
-                      <li style={{ textDecoration: "line-through" }} >Highlight your profile for greater visibility</li>
-                      <li style={{ textDecoration: "line-through" }}>View Kundalis for compatibility</li>
-                      <li style={{ textDecoration: "line-through" }}>Get Priority listing in search results</li>
-                    </ul>
-                    <button type="button" class="btn mt-5 px-5 py-2  membership_btn">Buy Now</button>
+                if (!isNaN(inputDays)) {
+                  const result = calculateMonthsAndDays(inputDays);
+                  if (result.months == 0) {
+                    var time = `${result.remainingDays} Days`;
+                  }
+                  else {
+                    time = `${result.months} Month ${result.remainingDays} Days`;
+                  }
+
+                } else {
+                  console.log("Invalid input. Please enter a valid number of days.");
+                }
+
+                return (<div className="col-lg-4 col-md-6 col-12 p-0 my-2 ">
+                  <div className='membership_main_col mx-1'> <div className="membership_col1 py-5">
+                    <h4 className='text-white'>{element.mp_name}</h4>
+                    <h4 className='text-white ' id='price'><sup>₹</sup>{element.mp_dis_price}<span>/ month</span></h4>
+                    <h4 id="membership_h4_3"
+                    // className={`${element.mp_price === element.mp_dis_price && "d-none"}`}
+                    ><sup>₹</sup>
+                      {element.mp_price}
+                    </h4>
                   </div>
-                </div></div>
-              )
-            }) : <h1>Data Not Found</h1>
+                    <div className="membership_col2 py-4">
+                      <ul >
+                        <li>View unlimited profiles</li>
+                        <li>Send unilimited Express interest invites</li>
+                        <li>{time}</li>
+                        <li>View {element?.mp_dir_contact_count} contact for direct communication</li>
+                        <li style={{ textDecoration: "line-through" }} >Highlight your profile for greater visibility</li>
+                        <li style={{ textDecoration: "line-through" }}>View Kundalis for compatibility</li>
+                        <li style={{ textDecoration: "line-through" }}>Get Priority listing in search results</li>
+                      </ul>
+                      <button type="button" class="btn mt-5 px-5 py-2  membership_btn">Buy Now</button>
+                    </div>
+                  </div></div>
+                )
+              }) : <h1>Data Not Found</h1>
           }
 
         </div>
